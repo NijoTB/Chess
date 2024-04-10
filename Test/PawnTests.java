3d22a9295f3d527db6f632213b24b449b2021c6c
NijoTB <161641373+NijoTB@users.noreply.github.com>
2024-04-10 16:53:25 -0400

    started the pawn

--- Test/PawnTests.java
+++ Test/PawnTests.java
@@ -5,5 +5,107 @@ import static org.junit.Assert.assertTrue;
 
 
 public class PawnTests {
+    @Test
+    public void pawnMoveUp(){
+        Board testBoard = new Board(false);
+        Move moveUp = new Move(4,1,4, 2);
 
+        Pawn moveTest = new Pawn( "pawn", ChessColor.BLACK, '♙' );
+        testBoard.display();
+        assertTrue(moveTest.canMove(moveUp));
+    }
+    @Test
+    public void pawnMoveUp2(){
+        Board testBoard = new Board(false);
+        Move moveUp = new Move(2,1,2, 3);
+
+        Pawn moveTest = new Pawn( "pawn", ChessColor.BLACK, '♙' );
+        testBoard.display();
+        assertTrue(moveTest.canMove(moveUp));
+    }
+
+
+
+    // can't go sideways tests
+    @Test
+    public void pawnMoveLeft(){
+        Board testBoard = new Board(false);
+        Move moveUp = new Move(4,3,1, 3);
+
+        Pawn moveTest = new Pawn( "pawn", ChessColor.BLACK, '♙' );
+        testBoard.display();
+        assertFalse(moveTest.canMove(moveUp));
+    }
+    @Test
+    public void pawnMoveRight(){
+        Board testBoard = new Board(false);
+        Move moveUp = new Move(3,2,6, 2);
+
+        Pawn moveTest = new Pawn( "pawn", ChessColor.BLACK, '♙' );
+        testBoard.display();
+        assertFalse(moveTest.canMove(moveUp));
+    }
+
+
+    // can't go backwards tests
+    @Test
+    public void pawnMoveBack(){
+        Board testBoard = new Board(false);
+        Move moveUp = new Move(5,3,5, 0);
+
+        Pawn moveTest = new Pawn( "pawn", ChessColor.BLACK, '♙' );
+        testBoard.display();
+        assertFalse(moveTest.canMove(moveUp));
+    }
+
+
+    // can't go diagonal tests
+    @Test
+    public void pawnMoveTopLeft(){
+        Board testBoard = new Board(false);
+        Move moveUp = new Move(7,0,0, 7);
+
+        Pawn moveTest = new Pawn( "pawn", ChessColor.BLACK, '♙' );
+        testBoard.display();
+        assertFalse(moveTest.canMove(moveUp));
+    }
+    @Test
+    public void pawnMoveTopRight(){
+        Board testBoard = new Board(false);
+        Move moveUp = new Move(0,0,7, 7);
+
+        Pawn moveTest = new Pawn( "pawn", ChessColor.BLACK, '♙' );
+        testBoard.display();
+        assertFalse(moveTest.canMove(moveUp));
+    }
+    @Test
+    public void pawnMoveBottomLeft(){
+        Board testBoard = new Board(false);
+        Move moveUp = new Move(7,7,0, 0);
+
+        Pawn moveTest = new Pawn( "pawn", ChessColor.BLACK, '♙' );
+        testBoard.display();
+        assertFalse(moveTest.canMove(moveUp));
+    }
+    @Test
+    public void pawnMoveBottomRight(){
+        Board testBoard = new Board(false);
+        Move moveUp = new Move(0,7,7, 0);
+
+        Pawn moveTest = new Pawn( "pawn", ChessColor.BLACK, '♙' );
+        testBoard.display();
+        assertFalse(moveTest.canMove(moveUp));
+    }
+
+
+    // can't go anywhere random tests
+    @Test
+    public void pawnMoveRandom(){
+        Board testBoard = new Board(false);
+        Move moveUp = new Move(7,7,0, 1);
+
+        Pawn moveTest = new Pawn( "pawn", ChessColor.BLACK, '♙' );
+        testBoard.display();
+        assertFalse(moveTest.canMove(moveUp));
+    }
 }
--- src/Pawn.java
+++ src/Pawn.java
@@ -1,4 +1,4 @@
-public class Pawn extends Piece{
+public class Pawn extends Piece {
     public Pawn(String id, ChessColor color, char symbol) {
         super(id, color, symbol);
     }
@@ -8,25 +8,37 @@ public class Pawn extends Piece{
         //if this happens the white or black pawn will have the option to move once or twice
         // but only on the first move. any move after is only one space.
         if (color == ChessColor.WHITE) {
-            if (move.nextX == move.currentX -1 && move.currentY == move.nextY) {
-                return true;
-            } else if (move.currentX == 6 && move.nextX == move.currentX -2 && move.currentY == move.nextY) {
+            if (move.nextY == move.currentY - 1 && move.currentX == move.nextX || move.currentY == 6 && move.nextY == move.currentX - 2 && move.currentX == move.nextX) {
+                if (Board.board[move.nextY][move.nextX].piece != null) {
+                    return false;
+                }
                 return true;
             }
+            if (Board.board[move.nextY][move.nextX].piece != null && Board.board[move.nextY][move.nextX].piece.color == ChessColor.BLACK) {
+                if (move.nextY == move.currentY - 1 && (move.nextX == move.currentX + 1 || move.nextX == move.currentX - 1)) {
+                    return true;
+                }
+
+            }
+
+            return false;
         }
 
         if (color == ChessColor.BLACK) {
-            if (move.nextX == move.currentX +1 && move.currentY == move.nextY) {
-                return true;
-            } else if (move.currentX == 1 && move.nextX == move.currentX +2 && move.currentY == move.nextY) {
+            if (move.nextY == move.currentY + 1 && move.currentX == move.nextX || move.currentY == 1 && move.nextY == move.currentY + 2 && move.currentX == move.nextX) {
+                if (Board.board[move.nextY][move.nextX].piece != null) {
+                    return false;
+                }
                 return true;
             }
+            if (Board.board[move.nextY][move.nextX].piece != null && Board.board[move.nextY][move.nextX].piece.color == ChessColor.WHITE) {
+                if (move.nextY == move.currentY + 1 && (move.nextX == move.currentX + 1 || move.nextX == move.currentX - 1)) {
+                    return true;
+                }
+            }
+            return false;
         }
         return false;
     }
-
-
-
-
     //♙ ♟
 }
