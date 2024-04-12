e088da410e57183b8d610570ceadb0b68e9be4b3
NijoTB <161641373+NijoTB@users.noreply.github.com>
2024-04-12 16:30:50 -0400

    start of game rules

--- src/GameState.java
+++ src/GameState.java
@@ -1,3 +1,4 @@
+import javax.xml.namespace.QName;
 import java.util.Arrays;
 import java.util.Optional;
 import java.util.Random;
@@ -7,4 +8,36 @@ public class GameState {
     public Player[] players;
     public Board board;
     public Player currentPlayer;
+
+
+    public GameState() {
+        this.players = new Player[2];
+        this.players[0] = new Player();
+        this.players[1] = new Player();
+        this.board = new Board();
+        this.currentPlayer = this.randomPlayer();
+    }
+
+
+    public void newGame() {
+        this.board = new Board();
+        this.currentPlayer = this.randomPlayer();
+    }
+
+
+    public Player randomPlayer() {
+        return this.players[new Random().nextBoolean() ? 0 : 1];
+    }
+
+
+    public void nextPlayer() {
+        if (this.currentPlayer.equals(this.players[0])) {
+            this.currentPlayer = this.players[1];
+        } else {
+            this.currentPlayer = this.players[0];
+        }
+    }
+
+
+
 }
--- src/Player.java
+++ src/Player.java
@@ -1,8 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Player {
 
+    public static final Map<String, Integer> inputMap = Map.of("a", 0, "b", 1, "c", 2, "d", 3, "e", 4, "f", 5, "g", 5, "h", 6, "i", 7);
+
+    Scanner scanner = new Scanner(System.in);
+
+
+    public String getInput() {
+        return scanner.nextLine();
+    }
+
+
+    public boolean validInput(String input) {
+        if (input.length() != 5) {
+            return false;
+        }
+        String startXStr = input.substring(0, 2);
+        String startYStr = input.substring(3, 5);
+        String endXStr = input.substring();
+        String endYStr = input.substring();
+
+
+    }
 
 
 }
